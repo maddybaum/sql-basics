befc6f23fdb2e2bc23c7457a64dc518fd705ead0
Maddy Baum <70274731+maddybaum@users.noreply.github.com>
2022-08-22 15:13:26 -0500

    Create artist.sql

---
+++ artist.sql
@@ -0,0 +1,7 @@
+SELECT name FROM artist ORDER BY name DESC LIMIT 10;
+
+SELECT name FROM artist ORDER BY name ASC LIMIT 5;
+
+SELECT name FROM artist WHERE name LIKE 'Black%';
+
+SELECT name FROM artist WHERE name LIKE '%Black%';
